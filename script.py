451018ce366343542a2003a11d5e8d1c7a934687
bunk0man <thomasckik@gmail.com>
2017-06-03 20:40:20 +0200

    post write

--- script.py
+++ script.py
@@ -3,3 +3,7 @@ import facebook
 graph = facebook.GraphAPI(access_token='your_token', version='2.9')
 # Get all the comments from a post
 comments = graph.get_connections(id='post_id', connection_name='comments')
+
+# Write 'Hello, world' to the active user's wall.
+graph.put_object(parent_object='me', connection_name='feed',
+                 message='Hello, world')
